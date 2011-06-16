8918bf45b6bcd9fe617986e9f0290cee56fe1182
Daniel Fernández <daniel.fernandez@11thlabs.org>
2011-06-16 21:51:02 +0000

    Added setCacheTTLMs to templateResolver configuration
    git-svn-id: https://thymeleaf.svn.sourceforge.net/svnroot/thymeleaf/trunk/thymeleafexamples-gtvg@289 18283e39-483d-46e1-a5f2-92434bed4d7e

--- src/main/java/thymeleafexamples/gtvg/web/application/GTVGApplication.java
+++ src/main/java/thymeleafexamples/gtvg/web/application/GTVGApplication.java
@@ -60,6 +60,8 @@ public class GTVGApplication {
         // This will convert "home" to "/WEB-INF/templates/home.html"
         templateResolver.setPrefix("/WEB-INF/templates/");
         templateResolver.setSuffix(".html");
+        // Set template cache TTL to 1 hour. If not set, entries would live in cache until expelled by LRU
+        templateResolver.setCacheTTLMs(Long.valueOf(3600000L));
         
         templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
