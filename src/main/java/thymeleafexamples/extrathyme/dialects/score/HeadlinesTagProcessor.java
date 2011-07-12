060b98a264bc764e1641e642423ff6e81416d304
Daniel Fernández <daniel.fernandez@11thlabs.org>
2011-07-12 18:41:41 +0000

    Several changes made
    git-svn-id: https://thymeleaf.svn.sourceforge.net/svnroot/thymeleaf/trunk/thymeleafexamples-extrathyme@497 18283e39-483d-46e1-a5f2-92434bed4d7e

--- src/main/java/thymeleafexamples/extrathyme/dialects/score/HeadlinesTagProcessor.java
+++ src/main/java/thymeleafexamples/extrathyme/dialects/score/HeadlinesTagProcessor.java
@@ -59,35 +59,60 @@ public class HeadlinesTagProcessor extends AbstractMarkupSubstitutionTagProcesso
             final TemplateResolution templateResolution, final Document document,
             final Element element) {
 
+        /*
+         * Obtain the Spring application context. Being a SpringMVC-based 
+         * application, we know that the IContext implementation being
+         * used is SpringWebContext, and so we can directly cast and ask it
+         * to return the AppCtx. 
+         */
         final ApplicationContext appCtx = 
             ((SpringWebContext)arguments.getContext()).getApplicationContext();
-        
+
+        /*
+         * Obtain the HeadlineRepository bean from the application context, and ask
+         * it for the current list of headlines.
+         */
         final HeadlineRepository headlineRepository = appCtx.getBean(HeadlineRepository.class);
         final List<Headline> headlines = headlineRepository.findAllHeadlines();
 
+        /*
+         * Read the 'order' attribute from the tag. This optional attribute in our tag 
+         * will allow us to determine whether we want to show a random headline or
+         * only the latest one ('latest' is default).
+         */
         final String order = element.getAttribute("order");
 
         String headlineText = null;
         if (order != null && order.trim().toLowerCase().equals("random")) {
+            // Order is random 
 
             final int r = this.rand.nextInt(headlines.size());
             headlineText = headlines.get(r).getText();
             
         } else {
-            // order is "latest", only the latest headline will be shown
+            // Order is "latest", only the latest headline will be shown
             
             Collections.sort(headlines);
             headlineText = headlines.get(headlines.size() - 1).getText();
             
         }
-        
 
+        /*
+         * Create the DOM structure that will be substituting our custom tag.
+         * The headline will be shown inside a '<div>' tag, and so this must
+         * be created first and then a Text node must be added to it.
+         */
         final Element container = document.createElement("div");
         container.setAttribute("class", "headlines");
 
         final Text text = document.createTextNode(headlineText);
         container.appendChild(text);
-        
+
+        /*
+         * The abstract IAttrProcessor implementation we are using defines
+         * that a list of nodes will be returned, and that these nodes
+         * will substitute the tag we are processing.
+         */
         final List<Node> nodes = new ArrayList<Node>();
         nodes.add(container);
         
