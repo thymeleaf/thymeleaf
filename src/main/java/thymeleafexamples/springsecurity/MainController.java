fcc872411e13182d3ef384acad9bbd135a1d7177
Daniel Fernández <daniel.fernandez@11thlabs.org>
2011-11-13 00:41:13 +0000

    Share project "thymeleafexamples-springsecurity" into "https://thymeleaf.svn.sourceforge.net/svnroot/thymeleaf"
    git-svn-id: https://thymeleaf.svn.sourceforge.net/svnroot/thymeleaf/trunk/thymeleafexamples-springsecurity@805 18283e39-483d-46e1-a5f2-92434bed4d7e

---
+++ src/main/java/thymeleafexamples/springsecurity/MainController.java
@@ -0,0 +1,82 @@
+package thymeleafexamples.springsecurity;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Application home page and login.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String root(Locale locale) {
+        return "redirect:/index.html";
+    }
+
+    /** Home page. */
+    @RequestMapping("/index.html")
+    public String index() {
+        return "index.html";
+    }
+
+    /** User zone index. */
+    @RequestMapping("/user/index.html")
+    public String userIndex() {
+        return "user/index.html";
+    }
+
+    /** Administration zone index. */
+    @RequestMapping("/admin/index.html")
+    public String adminIndex() {
+        return "admin/index.html";
+    }
+
+    /** Login form. */
+    @RequestMapping("/login.html")
+    public String login() {
+        return "login.html";
+    }
+
+    /** Login form with error. */
+    @RequestMapping("/login-error.html")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login.html";
+    }
+
+    /** Simulation of an exception. */
+    @RequestMapping("/simulateError.html")
+    public void simulateError() {
+        throw new RuntimeException("This is a simulated error message");
+    }
+
+    /** Substitute 'less than' and 'greater than' symbols by its HTML entities. */
+    private String escapeTags(String text) {
+        if (text == null) {
+            return null;
+        }
+        return text.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+    }
+
+    /** Error page. */
+    @RequestMapping("/error.html")
+    public String error(HttpServletRequest request, Model model) {
+        model.addAttribute("errorCode", "Error " + request.getAttribute("javax.servlet.error.status_code"));
+        Throwable throwable = (Throwable) request.getAttribute("javax.servlet.error.exception");
+        StringBuilder errorMessage = new StringBuilder();
+        errorMessage.append("<ul>");
+        while (throwable != null) {
+            errorMessage.append("<li>").append(escapeTags(throwable.getMessage())).append("</li>");
+            throwable = throwable.getCause();
+        }
+        errorMessage.append("</ul>");
+        model.addAttribute("errorMessage", errorMessage.toString());
+        return "error.html";
+    }
+}
