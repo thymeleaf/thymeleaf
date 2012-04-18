63e9c3ae04e4c9d0f1d3b4b382b58f738328c27b
Daniel Fernández <daniel.fernandez@11thlabs.org>
2012-04-18 23:14:43 +0000

    Several changes made
    git-svn-id: https://thymeleaf.svn.sourceforge.net/svnroot/thymeleaf/trunk/thymeleaf-2.0@1361 18283e39-483d-46e1-a5f2-92434bed4d7e

--- src/main/java/org/thymeleaf/standard/expression/OgnlExpressionEvaluator.java
+++ src/main/java/org/thymeleaf/standard/expression/OgnlExpressionEvaluator.java
@@ -22,6 +22,11 @@ package org.thymeleaf.standard.expression;
 import java.util.Collections;
 import java.util.Map;
 
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.LoaderClassPath;
+import ognl.Ognl;
 import ognl.OgnlException;
 
 import org.slf4j.Logger;
@@ -31,6 +36,8 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.cache.ICache;
 import org.thymeleaf.cache.ICacheManager;
 import org.thymeleaf.exceptions.TemplateProcessingException;
+import org.thymeleaf.util.ClassLoaderUtils;
+import org.thymeleaf.util.ObjectUtils;
 
 /**
  * 
@@ -49,6 +56,8 @@ public class OgnlExpressionEvaluator
     private static final Logger logger = LoggerFactory.getLogger(OgnlExpressionEvaluator.class);
 
 
+    private static boolean booleanFixApplied = false;
+    
     
     
     public final Object evaluate(final Arguments arguments, final String expression, final Object root) {
@@ -87,7 +96,7 @@ public class OgnlExpressionEvaluator
                 contextVariables.putAll(additionalContextVariables);
             }
             
-            return ognl.Ognl.getValue(expressionTree, contextVariables, root);
+            return Ognl.getValue(expressionTree, contextVariables, root);
             
         } catch (final OgnlException e) {
             throw new TemplateProcessingException(
@@ -113,6 +122,10 @@ public class OgnlExpressionEvaluator
     
     private OgnlExpressionEvaluator() {
         super();
+        if (!booleanFixApplied && shouldApplyOgnlBooleanFix()) {
+            applyOgnlBooleanFix();
+            booleanFixApplied = true;
+        }
     }
 
     
@@ -123,5 +136,84 @@ public class OgnlExpressionEvaluator
     public String toString() {
         return "OGNL";
     }
+    
+    
+    
+    /**
+     * <p>
+     *   Determines whether a fix should be applied to OGNL in order
+     *   to evaluate Strings as booleans in the same way as 
+     *   Thymeleaf does ('false', 'off' and 'no' are actually "false"
+     *   instead of OGNL's default "true"). 
+     * </p>
+     * 
+     * @return whether the OGNL boolean fix should be applied or not.
+     */
+    protected boolean shouldApplyOgnlBooleanFix() {
+        return true;
+    }
+        
+    
+    
+    private static void applyOgnlBooleanFix() {
+        
+        try {
+            
+            final ClassLoader classLoader = 
+                    ClassLoaderUtils.getClassLoader(OgnlExpressionEvaluator.class);
+            
+            final ClassPool pool = new ClassPool(true);
+            pool.insertClassPath(new LoaderClassPath(classLoader));
+
+            final CtClass[] params = new CtClass[] { pool.get(Object.class.getName()) };
+            
+            // We must load by class name here instead of "OgnlOps.class.getName()" because
+            // the latter would cause the class to be loaded and therefore it would not be
+            // possible to modify it.
+            final CtClass ognlClass = pool.get("ognl.OgnlOps");
+            final CtClass fixClass = pool.get(OgnlExpressionEvaluator.class.getName());
+            
+            final CtMethod ognlMethod = 
+                    ognlClass.getDeclaredMethod("booleanValue", params);
+            final CtMethod fixMethod = 
+                    fixClass.getDeclaredMethod("fixBooleanValue", params);
+            
+            ognlMethod.setBody(fixMethod, null);
+            
+            // Pushes the class to the class loader, effectively making it
+            // load the modified version instead of the original one. 
+            ognlClass.toClass(classLoader, null);
+            
+        } catch (final Exception e) {
+            // Any exceptions here will be consumed and converted into log messages.
+            // An exception at this point could be caused by multiple situations that 
+            // should not suppose the stop of the framework's initialization.
+            // If the fix cannot not applied, an INFO message is issued and initialization
+            // continues normally.
+            if (logger.isTraceEnabled()) {
+                logger.trace(
+                        "Thymeleaf was not able to apply a fix on OGNL's boolean evaluation " +
+                        "that would have enabled OGNL to evaluate Strings as booleans (e.g. in " +
+                        "\"th:if\") in exactly the same way as Thymeleaf itself or Spring EL ('false', " +
+                        "'off' and 'no' should be considered \"false\"). This did not stop the " +
+                        "initialization process.", e);
+            } else {
+                logger.info(
+                        "Thymeleaf was not able to apply a fix on OGNL's boolean evaluation " +
+                        "that would have enabled OGNL to evaluate Strings as booleans (e.g. in " +
+                        "\"th:if\") in exactly the same way as Thymeleaf itself or Spring EL ('false', " +
+                        "'off' and 'no' should be considered \"false\"). This did not stop the " +
+                        "initialization process. Exception raised was " + e.getClass().getName() + 
+                        ": " + e.getMessage() + " [Set the log to TRACE for the complete exception stack trace]");
+            }
+        }
+        
+    }
+        
+        
+    static boolean fixBooleanValue(final Object value) {
+        return ObjectUtils.evaluateAsBoolean(value);
+    }
+    
     
 }
---
+++ src/main/java/org/thymeleaf/util/ClassLoaderUtils.java
@@ -0,0 +1,77 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2012, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.util;
+
+
+/**
+ * <p>
+ *   Utility class for obtaining a correct classloader on which to operate from a
+ *   specific class.
+ * </p>
+ * 
+ * @author Daniel Fern&aacute;ndez
+ * 
+ * @since 2.0.6
+ *
+ */
+public final class ClassLoaderUtils {
+    
+    
+    
+    /**
+     * <p>
+     *   Try to obtain a classloader, following these priorities:
+     * </p>
+     * <ol>
+     *   <li>If there is a <i>thread context class loader</i>, return it.</li>
+     *   <li>Else if there is a class loader related to the class passed as argument, return it.</li>
+     *   <li>Else return the <i>system class loader.</li>
+     * </ol>
+     * 
+     * @param clazz the class which loader will be obtained in the second step. Can be null (that will
+     *              skip that second step).
+     * @return a non-null, safe classloader to use.
+     */
+    public static ClassLoader getClassLoader(final Class<?> clazz) {
+        // Context class loader can be null
+        final ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+        if (contextClassLoader != null) {
+            return contextClassLoader;
+        }
+        if (clazz != null) {
+            // The class loader for a specific class can also be null
+            final ClassLoader clazzClassLoader = clazz.getClassLoader();
+            if (clazzClassLoader != null) {
+                return clazzClassLoader;
+            }
+        }
+        // The only class loader we can rely on for not being null is the system one
+        return ClassLoader.getSystemClassLoader();
+    }
+    
+    
+    
+    private ClassLoaderUtils() {
+        super();
+    }
+    
+    
+    
+}
