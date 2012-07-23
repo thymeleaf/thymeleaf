9ae41258cc4cebf4b8d5c95e8195a9f2d62f2212
Daniel Fernández <daniel.fernandez@11thlabs.org>
2012-07-24 00:38:46 +0200

    Added ChainedFragmentSpec

---
+++ src/main/java/org/thymeleaf/fragment/ChainedFragmentSpec.java
@@ -0,0 +1,73 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2012, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.fragment;
+
+import java.util.List;
+
+import org.thymeleaf.Configuration;
+import org.thymeleaf.dom.Node;
+import org.thymeleaf.util.Validate;
+
+
+
+
+/**
+ * 
+ * @author Daniel Fern&aacute;ndez
+ * 
+ * @since 2.0.9
+ *
+ */
+public final class ChainedFragmentSpec implements IFragmentSpec {
+    
+    private final IFragmentSpec fragmentSpec1;
+    private final IFragmentSpec fragmentSpec2;
+    
+    
+    public ChainedFragmentSpec(
+            final IFragmentSpec fragmentSpec1, final IFragmentSpec fragmentSpec2) {
+        super();
+        Validate.notNull(fragmentSpec1, "Fragment spec 1 cannot be null");
+        Validate.notNull(fragmentSpec2, "Fragment spec 2 cannot be null");
+        this.fragmentSpec1 = fragmentSpec1;
+        this.fragmentSpec2 = fragmentSpec2;
+    }
+
+
+    
+    
+    
+    public final List<Node> extractFragment(final Configuration configuration, final List<Node> nodes) {
+        final List<Node> firstResult = this.fragmentSpec1.extractFragment(configuration, nodes);
+        return this.fragmentSpec2.extractFragment(configuration, firstResult);
+    }
+
+
+    
+    
+    @Override
+    public String toString() {
+        return "(FRAGMENT SPEC 1: " + this.fragmentSpec1 + " | FRAGMENT SPEC 2: " + this.fragmentSpec2 +")";
+    }
+
+    
+    
+}
+
