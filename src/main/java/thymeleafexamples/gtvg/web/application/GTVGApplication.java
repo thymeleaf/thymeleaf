e1c4238caf99c06faf78fc406930487474e87028
Daniel Fernández <daniel.fernandez@11thlabs.org>
2012-09-11 00:26:49 +0200

    Updated configuration comments.

--- src/main/java/thymeleafexamples/gtvg/web/application/GTVGApplication.java
+++ src/main/java/thymeleafexamples/gtvg/web/application/GTVGApplication.java
@@ -54,6 +54,7 @@ public class GTVGApplication {
     private static void initializeTemplateEngine() {
         
         ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        
         // XHTML is the default mode, but we will set it anyway for better understanding of code
         templateResolver.setTemplateMode("XHTML");
         // This will convert "home" to "/WEB-INF/templates/home.html"
@@ -61,6 +62,10 @@ public class GTVGApplication {
         templateResolver.setSuffix(".html");
         // Set template cache TTL to 1 hour. If not set, entries would live in cache until expelled by LRU
         templateResolver.setCacheTTLMs(Long.valueOf(3600000L));
+        
+        // Cache is set to true by default. Set to false if you want templates to
+        // be automatically updated when modified.
+        templateResolver.setCacheable(true);
         
         templateEngine = new TemplateEngine();
         templateEngine.setTemplateResolver(templateResolver);
