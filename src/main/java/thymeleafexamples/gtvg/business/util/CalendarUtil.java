0ad00b0d5375a12b1c7c82422d86fda9217568ad
Daniel Fernández <daniel.fernandez@11thlabs.org>
2013-05-02 22:09:29 +0200

    Minor calendar computation fix.

--- src/main/java/thymeleafexamples/gtvg/business/util/CalendarUtil.java
+++ src/main/java/thymeleafexamples/gtvg/business/util/CalendarUtil.java
@@ -31,7 +31,7 @@ public class CalendarUtil {
         
         final Calendar cal = Calendar.getInstance();
         cal.set(Calendar.YEAR, year);
-        cal.set(Calendar.MONTH, month + 1);
+        cal.set(Calendar.MONTH, month - 1);
         cal.set(Calendar.DAY_OF_MONTH, day);
         cal.set(Calendar.HOUR_OF_DAY, hour);
         cal.set(Calendar.MINUTE, minute);
