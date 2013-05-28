8bc10a3f1a1536e66bfdc94529b7c6e5ceaea63c
Daniel Fernández <daniel.fernandez@11thlabs.org>
2013-05-28 23:28:41 +0200

    Fixed th:field not being correctly applied when <input> has no "type" attribute.

--- src/main/java/org/thymeleaf/spring3/processor/attr/SpringInputGeneralFieldAttrProcessor.java
+++ src/main/java/org/thymeleaf/spring3/processor/attr/SpringInputGeneralFieldAttrProcessor.java
@@ -61,6 +61,7 @@ public final class SpringInputGeneralFieldAttrProcessor
     
     private static final String[] ALL_TYPE_ATTR_VALUES =
             new String[] {
+                null,
                 TEXT_INPUT_TYPE_ATTR_VALUE,
                 HIDDEN_INPUT_TYPE_ATTR_VALUE,
                 DATETIME_INPUT_TYPE_ATTR_VALUE,
