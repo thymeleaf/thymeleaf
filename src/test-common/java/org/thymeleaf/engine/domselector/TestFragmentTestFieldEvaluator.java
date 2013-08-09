a3a4a97725853c7fe9bc3786143bf19fa66e0689
Daniel Fernández <daniel.fernandez@11thlabs.org>
2013-08-09 18:05:05 +0200

    Added DOM selector tests

---
+++ src/test-common/java/org/thymeleaf/engine/domselector/TestFragmentTestFieldEvaluator.java
@@ -0,0 +1,56 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2012, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.engine.domselector;
+
+import org.thymeleaf.fragment.DOMSelectorFragmentSpec;
+import org.thymeleaf.fragment.IFragmentSpec;
+import org.thymeleaf.fragment.WholeFragmentSpec;
+import org.thymeleaf.testing.templateengine.resource.ITestResource;
+import org.thymeleaf.testing.templateengine.resource.ITestResourceResolver;
+import org.thymeleaf.testing.templateengine.standard.test.data.StandardTestEvaluatedField;
+import org.thymeleaf.testing.templateengine.standard.test.evaluator.field.defaultevaluators.AbstractStandardTestFieldEvaluator;
+
+
+public class TestFragmentTestFieldEvaluator extends AbstractStandardTestFieldEvaluator {
+
+
+    public static final IFragmentSpec DEFAULT_VALUE = WholeFragmentSpec.INSTANCE;
+
+
+    public TestFragmentTestFieldEvaluator() {
+        super(IFragmentSpec.class);
+    }
+
+
+    @Override
+    protected StandardTestEvaluatedField getValue(final String executionId, final ITestResource resource, 
+            final ITestResourceResolver testResourceResolver, 
+            final String fieldName, final String fieldQualifier, final String fieldValue) {
+
+        if (fieldValue == null || fieldValue.trim().equals("")) {
+            return StandardTestEvaluatedField.forDefaultValue(DEFAULT_VALUE);
+        }
+        
+        return StandardTestEvaluatedField.forSpecifiedValue(new DOMSelectorFragmentSpec(fieldValue.trim()));
+        
+    }
+    
+    
+}
---
+++ src/test-common/java/org/thymeleaf/engine/domselector/TestNodeReferenceChecker.java
@@ -0,0 +1,51 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2013, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.engine.domselector;
+
+import org.thymeleaf.dom.DOMSelector;
+import org.thymeleaf.dom.NestableAttributeHolderNode;
+import org.thymeleaf.dom.Node;
+
+
+public class TestNodeReferenceChecker implements DOMSelector.INodeReferenceChecker {
+
+    public static final String REF_ATTRIBUTE_NAME = "ref";
+
+    public TestNodeReferenceChecker() {
+        super();
+    }
+
+
+
+    public boolean checkReference(final Node node, final String referenceValue) {
+        if (node instanceof NestableAttributeHolderNode) {
+            final NestableAttributeHolderNode attributeHolderNode = (NestableAttributeHolderNode) node;
+            if (attributeHolderNode.hasAttribute(REF_ATTRIBUTE_NAME)) {
+                final String refValue = attributeHolderNode.getAttributeValue(REF_ATTRIBUTE_NAME);
+                if (referenceValue == null) {
+                    return (refValue == null);
+                }
+                return referenceValue.equals(refValue);
+            }
+        }
+        return false;
+    }
+
+}
