d24b104548654400e5944cbe8907721c7506dc46
Daniel Fernández <daniel.fernandez@11thlabs.org>
2013-09-02 22:16:46 +0200

    Tuned benchmark specification, added profile comparison benchmark

--- benchmark.sh
+++ benchmark.sh
@@ -1,3 +1,37 @@
-#/bin/sh
-mvn -P $1 clean compile test -Dtest=org.thymeleaf.benchmark.BenchmarkTest
+#!/bin/sh
+# Syntax: "./benchmark.sh [-t] {profile}
+
+if [ "$1" == '-t' ];
+then
+  SHOW_ONLY_TIME=true;
+else
+  SHOW_ONLY_TIME=false;
+fi
+
+if [ "$SHOW_ONLY_TIME" == 'true' ];
+then
+  PROFILE="$2";
+else
+  PROFILE="$1";
+fi
+
+if [ "$PROFILE" == '' ];
+then
+  echo 'Bad syntax: "./benchmark.sh [-t] {profile}"'
+  exit 1;
+fi
+
+COMMAND="mvn -P $PROFILE clean compile test -Dtest=org.thymeleaf.benchmark.BenchmarkTest"
+if [ "$SHOW_ONLY_TIME" == 'false' ];
+then
+  echo "
+Executing benchmark using profile: $PROFILE
+"
+  $COMMAND;
+else
+  EXPR='.*\[THYMELEAF\]\[\(.*\)\]\[.*'
+  OUTPUT=`$COMMAND`
+  TIME=$((`expr match "$OUTPUT" "$EXPR"`))
+  echo $TIME
+fi
 
---
+++ benchmark_compare.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+#
+# This script requires bash 4.0+ and awk
+#
+
+iter=10
+
+itermaxlen=`expr length $iter`
+timemaxlen=15
+profilenamemaxlen=0
+
+commandbase="./benchmark.sh -t "
+
+declare -A times
+
+for profile in "$@";
+do
+  times["$profile"]=0
+  len=`expr length $profile`
+  if [ "$profilenamemaxlen" -lt "$len" ];
+  then
+    profilenamemaxlen=$len
+  fi
+done
+
+for i in `seq 1 $iter`;
+do
+
+  for profile in "$@";
+  do
+    profiletime=${times["$profile"]}
+    time_nanos=`$commandbase $profile`
+	((profiletime+=time_nanos))
+    times["$profile"]=$profiletime
+	fi=`printf "%0"$itermaxlen"s" "$i"`
+	fprofile=`printf "%-"$profilenamemaxlen"s" "$profile"`
+	ftime=`printf "%"$timemaxlen"sns" "$time_nanos"`
+    echo "[$fi][$fprofile] $ftime"
+  done
+  
+done
+
+echo "
+----TOTAL TIMES----
+"
+
+smallestvalue=0
+smallestprofile=
+
+for profile in "$@";
+do
+  profiletime=${times["$profile"]}
+  if [ "$smallestvalue" == 0 ] || [ "$profiletime" -lt "$smallestvalue" ];
+  then
+    smallestvalue=$profiletime
+	smallestprofile=$profile
+  fi
+done
+
+for profile in "$@";
+do
+  profiletime=${times["$profile"]}
+  fprofile=`printf "%-"$profilenamemaxlen"s" "$profile"`
+  ftime=`printf "%"$timemaxlen"sns" "$profiletime"`
+  if [ "$profile" == "$smallestprofile" ];
+  then
+    echo "[$fprofile] $ftime *"
+  else
+	difference=`awk 'BEGIN{printf("%0.2f", '$profiletime' / '$smallestvalue' * 100)}'`
+    echo "[$fprofile] $ftime ($difference%)"
+  fi
+done
+
