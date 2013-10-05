7b843dc454207f77afd24610b4597f8a3898fe7f
Daniel Fernández <daniel.fernandez@11thlabs.org>
2013-10-05 23:44:53 +0200

    Tests for thymeleaf/thymeleaf#220. Avoided problems with BOM symbols in UTF-8 templates

---
+++ src/test-common/java/org/thymeleaf/parsing/Parsing01Test.java
@@ -0,0 +1,65 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2013, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.parsing;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring3.SpringTemplateEngine;
+import org.thymeleaf.templateparser.xmlsax.XhtmlAndHtml5NonValidatingSAXTemplateParser;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.testing.templateengine.util.ResourceUtils;
+import org.thymeleaf.util.ClassLoaderUtils;
+
+
+public class Parsing01Test {
+
+
+    public Parsing01Test() {
+        super();
+    }
+    
+    
+    
+    @Test
+    public void testParsing() throws Exception {
+
+        final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        final TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        final String result = templateEngine.process("parsing/parsingtest01.bulk", new Context());
+
+        final String expected = 
+                ResourceUtils.read(
+                        ClassLoaderUtils.getClassLoader(Parsing01Test.class).getResourceAsStream("parsing/parsingtest01-result.bulk"),
+                        "ISO-8859-1",
+                        true);
+        
+        Assert.assertEquals(expected, ResourceUtils.normalize(result));
+        
+    }
+    
+
+    
+}
---
+++ src/test-common/java/org/thymeleaf/parsing/Parsing02Test.java
@@ -0,0 +1,63 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2013, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.parsing;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.testing.templateengine.util.ResourceUtils;
+import org.thymeleaf.util.ClassLoaderUtils;
+
+
+public class Parsing02Test {
+
+
+    public Parsing02Test() {
+        super();
+    }
+    
+    
+    
+    @Test
+    public void testParsing() throws Exception {
+
+        final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setTemplateMode("HTML5");
+        templateResolver.setCharacterEncoding("UTF-16");
+
+        final TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        final String result = templateEngine.process("parsing/parsingtest02.bulk", new Context());
+
+        final String expected = 
+                ResourceUtils.read(
+                        ClassLoaderUtils.getClassLoader(Parsing02Test.class).getResourceAsStream("parsing/parsingtest02-result.bulk"),
+                        "ISO-8859-1",
+                        true);
+        
+        Assert.assertEquals(expected, ResourceUtils.normalize(result));
+        
+    }
+    
+
+    
+}
