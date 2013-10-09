a77ece9278a39e5c7c439f0b37701fea90ab63e2
Daniel Fernández <daniel.fernandez@11thlabs.org>
2013-10-10 00:45:52 +0200

    Made benchmark scripts more robust, computed averages instead of sums

--- benchmark.sh
+++ benchmark.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Syntax: "./benchmark.sh [-t] {profile}
+#
+# This script requires bash 4.0+ and awk
+#
 
 if [ "$1" == '-t' ];
 then
@@ -21,6 +24,10 @@ then
   exit 1;
 fi
 
+OUTPUT_FILE_DATE_SUFFIX=`date +"%Y%m%d%H%M%S"`
+OUTPUT_FILE_RANDOM_SUFFIX=`date | md5sum | cut -c1-6`
+OUTPUT_FILE="benchmark-output-$PROFILE-$OUTPUT_FILE_DATE_SUFFIX-$OUTPUT_FILE_RANDOM_SUFFIX"
+
 COMMAND="mvn -P $PROFILE clean compile test -Dtest=org.thymeleaf.benchmark.BenchmarkTest"
 if [ "$SHOW_ONLY_TIME" == 'false' ];
 then
@@ -30,8 +37,15 @@ Executing benchmark using profile: $PROFILE
   $COMMAND;
 else
   EXPR='.*\[THYMELEAF\]\[\(.*\)\]\[.*'
-  OUTPUT=`$COMMAND`
+  $COMMAND > $OUTPUT_FILE;
+  OUTPUT=`cat $OUTPUT_FILE`
   TIME=$((`expr match "$OUTPUT" "$EXPR"`))
+  if [ "$TIME" == "0" ];
+  then
+    $(mv $OUTPUT_FILE "$OUTPUT_FILE-error.log")
+    echo 0
+    exit 1
+  fi
+  rm $OUTPUT_FILE
   echo $TIME
 fi
-
--- benchmark_compare.sh
+++ benchmark_compare.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+# Syntax: "./benchmark_compare.sh {times} {profile} ({profile})*
 #
 # This script requires bash 4.0+ and awk
 #
 
-iter=5
+iter=$1
+shift   # eat $1
 
 itermaxlen=`expr length $iter`
 timemaxlen=15
@@ -11,11 +13,18 @@ profilenamemaxlen=0
 
 commandbase="./benchmark.sh -t "
 
+
+#
+# INITIALIZE VARIABLES
+#
+
 declare -A times
+declare -A iters
 
 for profile in "$@";
 do
   times["$profile"]=0
+  iters["$profile"]=0
   len=`expr length $profile`
   if [ "$profilenamemaxlen" -lt "$len" ];
   then
@@ -23,51 +32,89 @@ do
   fi
 done
 
+
+
+#
+# EXECUTE BENCHMARK $iter TIMES FOR EACH $profile
+#
+
 for i in `seq 1 $iter`;
 do
-
   for profile in "$@";
   do
     profiletime=${times["$profile"]}
-    time_nanos=`$commandbase $profile`
-	((profiletime+=time_nanos))
-    times["$profile"]=$profiletime
-	fi=`printf "%0"$itermaxlen"s" "$i"`
-	fprofile=`printf "%-"$profilenamemaxlen"s" "$profile"`
-	ftime=`printf "%"$timemaxlen"sns" "$time_nanos"`
-    echo "[$fi][$fprofile] $ftime"
+    profileiters=${iters["$profile"]}
+    time_nanos="$($commandbase $profile)"
+    if [ "$time_nanos" != "0" ];
+    then
+      ((profiletime+=time_nanos))
+      ((profileiters+=1))
+      times["$profile"]=$profiletime
+      iters["$profile"]=$profileiters
+      fi=`printf "%0"$itermaxlen"s" "$i"`
+      fprofile=`printf "%-"$profilenamemaxlen"s" "$profile"`
+      ftime=`printf "%"$timemaxlen"sns" "$time_nanos"`
+      echo "[$fi][$fprofile] $ftime"
+    fi
   done
-  
 done
 
-echo "
-----TOTAL TIMES----
-"
 
-smallestvalue=0
-smallestprofile=
 
+
+#
+# COMPUTE AVERAGE TIMES
+#
+
+declare -A avgtimes
 for profile in "$@";
 do
   profiletime=${times["$profile"]}
-  if [ "$smallestvalue" == 0 ] || [ "$profiletime" -lt "$smallestvalue" ];
+  profileiters=${iters["$profile"]}
+  profileavg=`awk 'BEGIN{printf("%0.2f", '$profiletime' / '$profileiters')}'`
+  avgtimes["$profile"]=$profileavg
+done
+
+
+
+#
+# COMPUTE SMALLEST (REFERENCE) VALUE
+#
+
+smallestvalue=0.0
+smallestprofile=
+for profile in "$@";
+do
+  profileavg=${avgtimes["$profile"]}
+  iszero=$(awk 'BEGIN{ print "'$smallestvalue'" == "0.0" }')
+  cond=$(awk 'BEGIN{ print "'$profileavg'" < "'$smallestvalue'" }')
+  if [ "$iszero" == 1 ] || [ "$cond" == 1 ];
   then
-    smallestvalue=$profiletime
+    smallestvalue=$profileavg
 	smallestprofile=$profile
   fi
 done
 
+
+#
+# OUTPUT RESULTS
+#
+
+echo "
+----TOTAL TIMES----
+"
 for profile in "$@";
 do
-  profiletime=${times["$profile"]}
+  profileavg=${avgtimes["$profile"]}
+  profileiters=${iters["$profile"]}
   fprofile=`printf "%-"$profilenamemaxlen"s" "$profile"`
-  ftime=`printf "%"$timemaxlen"sns" "$profiletime"`
+  ftime=`printf "%"$timemaxlen"sns" "$profileavg"`
   if [ "$profile" == "$smallestprofile" ];
   then
-    echo "[$fprofile] $ftime *"
+    echo "[$fprofile][$profileiters] $ftime *"
   else
-	difference=`awk 'BEGIN{printf("%0.2f", '$profiletime' / '$smallestvalue' * 100)}'`
-    echo "[$fprofile] $ftime ($difference%)"
+	difference=`awk 'BEGIN{printf("%0.2f", '$profileavg' / '$smallestvalue' * 100)}'`
+    echo "[$fprofile][$profileiters] $ftime ($difference%)"
   fi
 done
 
