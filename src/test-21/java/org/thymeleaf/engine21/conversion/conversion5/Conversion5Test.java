e0094da7570d43932da559a527109d1a13105358
Daniel Fernández <daniel.fernandez@11thlabs.org>
2013-10-16 00:39:00 +0200

    Tests for #conversions utility object

---
+++ src/test-21/java/org/thymeleaf/engine21/conversion/conversion5/Conversion5Test.java
@@ -0,0 +1,59 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2013, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.engine21.conversion.conversion5;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.engine21.conversion.conversion3.Conversion3WebProcessingContextBuilder;
+import org.thymeleaf.spring3.dialect.SpringStandardDialect;
+import org.thymeleaf.testing.templateengine.engine.TestExecutor;
+
+
+public class Conversion5Test {
+
+
+
+    public Conversion5Test() {
+        super();
+    }
+
+
+
+
+
+
+    @Test
+    public void testConversion() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.setProcessingContextBuilder(new Conversion3WebProcessingContextBuilder());
+        executor.setDialects(Arrays.asList(new IDialect[]{new ConversionTestDialect5()}));
+
+        executor.execute("classpath:engine21/conversion/conversion5");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+}
