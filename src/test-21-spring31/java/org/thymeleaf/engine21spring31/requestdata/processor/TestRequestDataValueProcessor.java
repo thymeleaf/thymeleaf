8d1bb90483723871739b9d17421387bb6a36527e
Daniel Fernández <daniel.fernandez@11thlabs.org>
2013-11-03 13:51:32 +0100

    Added new profile for Thymeleaf2.1+Spring3.1 and first RequestDataValueProcessor tests for thymeleaf/thymeleaf-spring3#7

---
+++ src/test-21-spring31/java/org/thymeleaf/engine21spring31/requestdata/processor/TestRequestDataValueProcessor.java
@@ -0,0 +1,52 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.engine21spring31.requestdata.processor;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.support.RequestDataValueProcessor;
+
+
+public class TestRequestDataValueProcessor implements RequestDataValueProcessor {
+
+
+    public String processAction(HttpServletRequest request, String action) {
+        return action;
+    }
+
+    public String processFormFieldValue(HttpServletRequest request, String name, String value, String type) {
+        return value;
+    }
+
+    public Map<String, String> getExtraHiddenFields(HttpServletRequest request) {
+        final Map<String,String> map = new LinkedHashMap<String,String>();
+        map.put("extra-field1", "The value of the First Extra field");
+        map.put("extra-field2", "The value of the Second Extra field");
+        return map;
+    }
+
+    public String processUrl(HttpServletRequest request, String url) {
+        return url;
+    }
+}
