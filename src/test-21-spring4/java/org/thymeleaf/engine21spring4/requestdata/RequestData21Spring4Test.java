b05d704199515127b388a366ce7778839b35c6f7
Daniel Fernandez <daniel.fernandez@11thlabs.org>
2013-12-17 02:29:18 +0100

    Added tests and test profiling for Spring 4

---
+++ src/test-21-spring4/java/org/thymeleaf/engine21spring4/requestdata/RequestData21Spring4Test.java
@@ -0,0 +1,106 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2012, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.engine21spring4.requestdata;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.spring4.dialect.SpringStandardDialect;
+import org.thymeleaf.testing.templateengine.context.web.SpringWebProcessingContextBuilder;
+import org.thymeleaf.testing.templateengine.engine.TestExecutor;
+
+
+public class RequestData21Spring4Test {
+
+
+    public RequestData21Spring4Test() {
+        super();
+    }
+    
+    
+    
+    
+    @Test
+    public void testFormWith() throws Exception {
+
+        final SpringWebProcessingContextBuilder contextBuilder = new SpringWebProcessingContextBuilder();
+        contextBuilder.setApplicationContextConfigLocation("classpath:engine21spring4/requestdata/applicationContext-with.xml");
+
+        final TestExecutor executor = new TestExecutor();
+        executor.setProcessingContextBuilder(contextBuilder);
+        executor.setDialects(Arrays.asList(new IDialect[] { new SpringStandardDialect()}));
+        executor.execute("classpath:engine21spring4/requestdata/formwith");
+        
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+    @Test
+    public void testFormWithout() throws Exception {
+
+        final SpringWebProcessingContextBuilder contextBuilder = new SpringWebProcessingContextBuilder();
+        contextBuilder.setApplicationContextConfigLocation("classpath:engine21spring4/requestdata/applicationContext-without.xml");
+
+        final TestExecutor executor = new TestExecutor();
+        executor.setProcessingContextBuilder(contextBuilder);
+        executor.setDialects(Arrays.asList(new IDialect[] { new SpringStandardDialect()}));
+        executor.execute("classpath:engine21spring4/requestdata/formwithout");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+
+    @Test
+    public void testUrlsWith() throws Exception {
+
+        final SpringWebProcessingContextBuilder contextBuilder = new SpringWebProcessingContextBuilder();
+        contextBuilder.setApplicationContextConfigLocation("classpath:engine21spring4/requestdata/applicationContext-with.xml");
+
+        final TestExecutor executor = new TestExecutor();
+        executor.setProcessingContextBuilder(contextBuilder);
+        executor.setDialects(Arrays.asList(new IDialect[] { new SpringStandardDialect()}));
+        executor.execute("classpath:engine21spring4/requestdata/urlswith");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+    @Test
+    public void testUrlsWithout() throws Exception {
+
+        final SpringWebProcessingContextBuilder contextBuilder = new SpringWebProcessingContextBuilder();
+        contextBuilder.setApplicationContextConfigLocation("classpath:engine21spring4/requestdata/applicationContext-without.xml");
+
+        final TestExecutor executor = new TestExecutor();
+        executor.setProcessingContextBuilder(contextBuilder);
+        executor.setDialects(Arrays.asList(new IDialect[] { new SpringStandardDialect()}));
+        executor.execute("classpath:engine21spring4/requestdata/urlswithout");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+}
---
+++ src/test-21-spring4/java/org/thymeleaf/engine21spring4/requestdata/model/FormBean01.java
@@ -0,0 +1,77 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.engine21spring4.requestdata.model;
+
+import java.util.Date;
+
+public class FormBean01 {
+
+    private Integer id = null;
+    private String name = null;
+    private Date date = null;
+    private boolean valid = false;
+    
+    
+    public FormBean01() {
+        super();
+    }
+
+
+    public Integer getId() {
+        return this.id;
+    }
+
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Date getDate() {
+        return this.date;
+    }
+
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public void setDate(final Date date) {
+        this.date = date;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(final boolean valid) {
+        this.valid = valid;
+    }
+
+    @Override
+    public String toString() {
+        return "FormBean01 [name=" + this.name + ", date=" + this.date + "]";
+    }
+    
+}
