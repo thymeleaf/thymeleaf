848b93919deea9e07fc9f0b00207d77b203d7fc6
Daniel Fernández <daniel.fernandez@11thlabs.org>
2014-09-18 01:27:11 +0200

    Minor modification on block item parsing.

--- src/main/java/org/thymeleaf/engine/markup/handler/BlockSelectorItem.java
+++ src/main/java/org/thymeleaf/engine/markup/handler/BlockSelectorItem.java
@@ -60,10 +60,25 @@ final class BlockSelectorItem {
             final boolean anyLevel = (pos == start + 2 || pos == start); // else, there's only one '/'
             start = pos;
 
-            while (pos < blockSelectorLen && blockSelector.charAt(pos) != '/') { pos++; }
+            int attrLevel = 0;
+            while (pos < blockSelectorLen) {
+
+                final char c = blockSelector.charAt(pos);
+
+                if (c == '/' && attrLevel == 0) {
+                    break;
+                } else if (c == '[') {
+                    attrLevel++;
+                } else if (c == ']') {
+                    attrLevel--;
+                }
+
+                pos++;
+
+            }
 
             final String item = blockSelector.substring(start, pos);
-            items.add(new BlockSelectorItem(anyLevel, (caseSensitive? item : item.toLowerCase())));
+            items.add(parseBlockItem(anyLevel, item, caseSensitive));
 
         }
 
@@ -76,4 +91,96 @@ final class BlockSelectorItem {
     }
 
 
+
+    static BlockSelectorItem parseBlockItem(
+            final boolean anyLevel, final String blockItem, final boolean caseSensitive) {
+
+
+
+        return new BlockSelectorItem(anyLevel, (caseSensitive ? blockItem : blockItem.toLowerCase()));
+
+    }
+
+
+
+/*
+    private static final class AttributeCondition {
+
+        static enum Operator {
+            EQUALS, NOT_EQUALS, STARTS_WITH, ENDS_WITH;
+
+            static Operator parse(final String operatorStr) {
+                if (operatorStr == null) {
+                    return null;
+                }
+                if ("=".equals(operatorStr)) {
+                    return EQUALS;
+                }
+                if ("!=".equals(operatorStr)) {
+                    return NOT_EQUALS;
+                }
+                if ("^=".equals(operatorStr)) {
+                    return STARTS_WITH;
+                }
+                if ("$=".equals(operatorStr)) {
+                    return ENDS_WITH;
+                }
+                return null;
+            }
+
+            static String[] extractOperator(final String specification) {
+                final int equalsPos = specification.indexOf('=');
+                if (equalsPos == -1) {
+                    return new String[] {specification.trim(), null, null};
+                }
+                final char cprev = specification.charAt(equalsPos - 1);
+                switch (cprev) {
+                    case '!':
+                        return new String[] {
+                                specification.substring(0, equalsPos - 1).trim(), "!=",
+                                specification.substring(equalsPos + 1).trim()};
+                    case '^':
+                        return new String[] {
+                                specification.substring(0, equalsPos - 1).trim(), "^=",
+                                specification.substring(equalsPos + 1).trim()};
+                    case '$':
+                        return new String[] {
+                                specification.substring(0, equalsPos - 1).trim(), "$=",
+                                specification.substring(equalsPos + 1).trim()};
+                    default:
+                        return new String[] {
+                                specification.substring(0, equalsPos).trim(), "=",
+                                specification.substring(equalsPos + 1).trim()};
+                }
+            }
+
+        }
+
+
+        private final String name;
+        private final Operator operator;
+        private final String value;
+
+        AttributeCondition(final String name, final Operator operator, final String value) {
+            super();
+            this.name = name;
+            this.operator = operator;
+            this.value = value;
+        }
+
+        String getName() {
+            return this.name;
+        }
+
+        Operator getOperator() {
+            return this.operator;
+        }
+
+        String getValue() {
+            return this.value;
+        }
+
+    }
+
+*/
 }
