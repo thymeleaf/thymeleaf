11d97994e81faec8f4764a6a62de34abddbf36cc
Daniel Fernández <daniel.fernandez@11thlabs.org>
2015-05-22 01:11:37 +0200

    Fixed null type in general Spring input processor (nulls are no longer allowed as wildcards)

--- thymeleaf-spring4/src/main/java/org/thymeleaf/spring4/processor/SpringInputGeneralFieldTagProcessor.java
+++ thymeleaf-spring4/src/main/java/org/thymeleaf/spring4/processor/SpringInputGeneralFieldTagProcessor.java
@@ -60,7 +60,6 @@ public final class SpringInputGeneralFieldTagProcessor
     
     private static final String[] ALL_TYPE_ATTR_VALUES =
             new String[] {
-                null,
                 TEXT_INPUT_TYPE_ATTR_VALUE,
                 HIDDEN_INPUT_TYPE_ATTR_VALUE,
                 DATETIME_INPUT_TYPE_ATTR_VALUE,
