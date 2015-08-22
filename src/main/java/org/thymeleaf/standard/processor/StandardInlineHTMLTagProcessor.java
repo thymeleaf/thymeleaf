8f6dde2b22d0fdd5c4d95934fed8e4cd0adb4d14
Daniel Fernández <daniel.fernandez@11thlabs.org>
2015-08-22 22:54:57 +0200

    Forwarded "dart" inlining to "javascript" and marked "dart" as deprecated.

--- src/main/java/org/thymeleaf/standard/processor/StandardInlineHTMLTagProcessor.java
+++ src/main/java/org/thymeleaf/standard/processor/StandardInlineHTMLTagProcessor.java
@@ -19,6 +19,9 @@
  */
 package org.thymeleaf.standard.processor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.ITemplateProcessingContext;
 import org.thymeleaf.engine.AttributeName;
 import org.thymeleaf.exceptions.TemplateProcessingException;
@@ -40,6 +43,8 @@ import org.thymeleaf.templatemode.TemplateMode;
  */
 public final class StandardInlineHTMLTagProcessor extends AbstractStandardTextInlineSettingTagProcessor {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StandardInlineHTMLTagProcessor.class);
+
     public static final int PRECEDENCE = 1000;
     public static final String ATTR_NAME = "inline";
 
@@ -60,6 +65,7 @@ public final class StandardInlineHTMLTagProcessor extends AbstractStandardTextIn
         final String inliner = (expressionResult == null? null : expressionResult.toString().toLowerCase());
 
         if (inliner != null) {
+
             if (INLINE_MODE_NONE.equals(inliner)) {
                 return NoOpInliner.INSTANCE;
             } else if (INLINE_MODE_HTML.equals(inliner)) {
@@ -71,6 +77,13 @@ public final class StandardInlineHTMLTagProcessor extends AbstractStandardTextIn
             } else if (INLINE_MODE_CSS.equals(inliner)) {
                 return StandardCSSInliner.INSTANCE;
             }
+
+            // Check DART inlining, deprecated in 3.0.0 (defaults to JAVASCRIPT)
+            final IInliner dartInliner = checkDartInline(inliner, tag.getLine(), tag.getCol());
+            if (dartInliner != null) {
+                return dartInliner;
+            }
+
         }
 
         throw new TemplateProcessingException(
@@ -83,6 +96,31 @@ public final class StandardInlineHTMLTagProcessor extends AbstractStandardTextIn
     }
 
 
+    /**
+     * Check for the pre-3.0 "dart" inlining mode, which was removed when inlining modes were matched to template modes,
+     * due to the inexistence of a (unneeded) "DART" template mode.
+     *
+     * NOTE this method is expressed here separately only in order to mark it as DEPRECATED and make it easily locatable
+     *      as such.
+     *
+     * @param inliner the name of the inliner we are asking for
+     * @return StandardJavaScriptInliner.INSTANCE if the inliner being asked for is "dart", null otherwise
+     * @deprecated in 3.0.0, "javascript" inlining should be used instead (since 3.0.0 all inlining modes refer to
+     *             template modes, and there is no "DART" template mode). Support for this default operation will be
+     *             removed in 3.1
+     */
+    @Deprecated
+    private static IInliner checkDartInline(final String inliner, final int line, final int col) {
+        if ("dart".equalsIgnoreCase(inliner)) {
+            LOGGER.warn(
+                    "[THYMELEAF][{}] Found inline call with value \"dart\", which has been deprecated as no " +
+                    "corresponding template mode exists for it. Inline will be redirected to \"javascript\", which " +
+                    "should now be used instead. This redirection will be removed in future versions of Thymeleaf.",
+                    TemplateEngine.threadIndex());
+            return StandardJavaScriptInliner.INSTANCE;
+        }
+        return null;
+    }
 
 
 }
