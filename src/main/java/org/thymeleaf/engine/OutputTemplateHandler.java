c3ff99bdc6d0cd86ea8900d9d9476a1ee8ac9bc0
Daniel Fernández <daniel.fernandez@11thlabs.org>
2015-09-05 20:09:53 +0200

    Minor comment fix

--- src/main/java/org/thymeleaf/engine/OutputTemplateHandler.java
+++ src/main/java/org/thymeleaf/engine/OutputTemplateHandler.java
@@ -45,6 +45,8 @@ public final class OutputTemplateHandler extends AbstractTemplateHandler {
     private final Writer writer;
 
 
+    // TODO OutputTemplateHandler should be configurable, and the logic of how an event should be written should not live in the events themselves
+
 
     /**
      * <p>
