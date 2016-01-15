e4304cb436b255487792497064c3f79b6d5ad4db
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-01-16 00:44:50 +0100

    Improved parsing of the ~ symbol as a part of a token

--- src/main/java/org/thymeleaf/standard/expression/Token.java
+++ src/main/java/org/thymeleaf/standard/expression/Token.java
@@ -61,8 +61,11 @@ public abstract class Token extends SimpleExpression {
     public static boolean isTokenChar(final String context, final int pos) {
 
         /*
-         * TOKEN chars: A-Za-z0-9[]._~ (and '-' in some contexts)
+         * TOKEN chars: A-Za-z0-9[]._ (plus '~' and '-' in some contexts)
          * (additionally, also, a series of internationalized characters: accents, other alphabets, etc.)
+         *
+         * '~' can be a part of a token as long as it is not followed by the '{' symbol, in which case it will
+         *     be considered to be the start of a fragment expression
          *
          * '-' can also be a numeric operator, so it will only be considered a token char if:
          *    * there are immediately previous chars which we can consider a token, but not a numeric token
@@ -96,7 +99,17 @@ public abstract class Token extends SimpleExpression {
         /*
          * Some more (less common) true's
          */
-        if (c == '[' || c == ']' || c == '.' || c == '_' || c == '~') {
+        if (c == '[' || c == ']' || c == '.' || c == '_') {
+            return true;
+        }
+        /*
+         * A special case: the tilde
+         */
+        if (c == '~') {
+            final int contextLen = context.length();
+            if (pos + 1 < contextLen) {
+                return (context.charAt(pos + 1) != '{');
+            }
             return true;
         }
         /*
