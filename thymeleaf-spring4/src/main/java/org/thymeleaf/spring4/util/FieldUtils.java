5018a727c4ae21e2601b0e3b18e083db1123b577
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-02-12 16:30:23 +0100

    Fixes #88. Improve error handling when errors cannot bind to the correct form bean
    Conflicts:
    	thymeleaf-spring3/src/main/java/org/thymeleaf/spring3/util/FieldUtils.java
    	thymeleaf-spring4/src/main/java/org/thymeleaf/spring4/util/FieldUtils.java

--- thymeleaf-spring4/src/main/java/org/thymeleaf/spring4/util/FieldUtils.java
+++ thymeleaf-spring4/src/main/java/org/thymeleaf/spring4/util/FieldUtils.java
@@ -238,10 +238,15 @@ public final class FieldUtils {
 
 
 
-    private static boolean checkErrors(final Configuration configuration, 
+    private static boolean checkErrors(final Configuration configuration,
             final IProcessingContext processingContext, final String expression) {
-        final BindStatus bindStatus =
-                FieldUtils.getBindStatus(configuration, processingContext, expression);
+        final BindStatus bindStatus = FieldUtils.getBindStatus(configuration, processingContext, expression);
+        if (bindStatus == null) {
+            throw new TemplateProcessingException(
+                    "Could not bind form errors using expression \"" + expression + "\". Please check this " +
+                    "expression is being executed inside the adequate context (e.g. a <form> with a th:object " +
+                    "attribute)");
+        }
         return bindStatus.isError();
     }
 
