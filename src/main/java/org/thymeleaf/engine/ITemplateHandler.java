f5a64bde49dc6a9171758da8adf64a02ee4379fb
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-03-23 22:15:40 +0100

    Removed ITemplaetHandlerStatus and associated structures

--- src/main/java/org/thymeleaf/engine/ITemplateHandler.java
+++ src/main/java/org/thymeleaf/engine/ITemplateHandler.java
@@ -45,8 +45,6 @@ public interface ITemplateHandler {
 
     public void setContext(final ITemplateContext context);
 
-    public void setStatus(final ITemplateHandlerStatus status);
-
 
     public void handleTemplateStart(final ITemplateStart templateStart);
 
@@ -73,9 +71,6 @@ public interface ITemplateHandler {
 
 
     public void handleProcessingInstruction(final IProcessingInstruction processingInstruction);
-
-
-    public void handlePending();
 
 
 }
