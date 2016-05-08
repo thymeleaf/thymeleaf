78ae90065126c2ba584276f6b4f194419eeed0a1
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-05-08 02:37:49 +0200

    Updated example application to Thymeleaf 3.0

---
+++ README.markdown
@@ -0,0 +1,37 @@
+
+Thymeleaf Examples: Thymeleaf vs JSP
+------------------------------------
+ 
+ This is an example project for the "Spring MVC view layer: Thymeleaf vs JSP" article
+ 
+ To learn more:
+ 
+     http://www.thymeleaf.org
+
+
+     
+Building
+--------
+ 
+ To build this project you will need Maven 2. You can get it at:
+ 
+     http://maven.apache.org
+
+ Clean compilation products:
+ 
+     mvn clean
+     
+ Compile:
+ 
+     mvn compile
+     
+ Run in a tomcat server:
+ 
+     mvn tomcat7:run
+
+ Once started, the application should be available at:
+ 
+     http://localhost:8080/thvsjsp
+
+
+ 
---
+++ src/main/java/thymeleafexamples/thvsjsp/application/SpringWebConfig.java
@@ -0,0 +1,137 @@
+/*
+ * =============================================================================
+ *
+ *   Copyright (c) 2011-2016, The THYMELEAF team (http://www.thymeleaf.org)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ * =============================================================================
+ */
+package thymeleafexamples.thvsjsp.application;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("thymeleafexamples.thvsjsp")
+public class SpringWebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    public static final String CHARACTER_ENCODING = "UTF-8";
+
+
+    private ApplicationContext applicationContext;
+
+
+
+    public SpringWebConfig() {
+        super();
+    }
+
+
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+
+
+    /**
+     *  Message externalization/internationalization
+     */
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setBasename("Messages");
+        return resourceBundleMessageSource;
+    }
+
+    /* **************************************************************** */
+    /*  THYMELEAF-SPECIFIC ARTIFACTS                                    */
+    /*  TemplateResolver <- TemplateEngine <- ViewResolver              */
+    /* **************************************************************** */
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        // Template cache is true by default. Set to false if you want
+        // templates to be automatically updated when modified.
+        templateResolver.setCacheable(true);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setViewNames(new String[] {"index","*th"});
+        viewResolver.setOrder(Integer.valueOf(1));
+        return viewResolver;
+    }
+
+    /* **************************************************************** */
+    /*  JSP-SPECIFIC ARTIFACTS                                          */
+    /* **************************************************************** */
+
+    @Bean
+    public InternalResourceViewResolver jspViewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/templates/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewNames(new String[] {"*jsp"});
+        viewResolver.setOrder(Integer.valueOf(2));
+        return viewResolver;
+    }
+
+    /* ******************************************************************* */
+    /*  Defines callback methods to customize the Java-based configuration */
+    /*  for Spring MVC enabled via {@code @EnableWebMvc}                   */
+    /* ******************************************************************* */
+
+    /**
+     *  Dispatcher configuration for serving static resources
+     */
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
+
+}
