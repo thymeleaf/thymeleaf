51f6132bb90c65f2b6d3c1d5f13c7afd4ddccd03
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-07-22 01:13:33 +0200

    Started evalution of app as a Boot a

--- src/main/java/thymeleafsandbox/springreactive/application/Application.java
+++ src/main/java/thymeleafsandbox/springreactive/application/Application.java
@@ -20,77 +20,17 @@
 package thymeleafsandbox.springreactive.application;
 
 
-import java.util.concurrent.CompletableFuture;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.http.server.reactive.HttpHandler;
-import org.springframework.http.server.reactive.boot.HttpServer;
-import org.springframework.http.server.reactive.boot.ReactorHttpServer;
-import org.springframework.web.reactive.DispatcherHandler;
-import org.springframework.web.reactive.ResponseStatusExceptionHandler;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
+@SpringBootApplication
 public class Application {
 
 
-    public static void main(String[] args) throws Exception {
-
-        /*
-         * Initialization: ApplicationContext
-         */
-        final AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(SpringReactiveWebConfig.class, SpringReactiveAppConfig.class);
-
-        /*
-         * Initialization: DispatcherHandler
-         */
-        final DispatcherHandler dispatcherHandler = new DispatcherHandler();
-        dispatcherHandler.setApplicationContext(context);
-
-        /*
-         * Initialization: CharacterEncodingFilter
-         */
-        // TODO * Apparently no implementation of WebFilter for CharacterEncoding yet (if such thing is needed)
-        final WebFilter[] filters = new WebFilter[] {};
-
-        /*
-         * Initialization HttpHandler
-         */
-        final HttpHandler httpHandler =
-                WebHttpHandlerBuilder.webHandler(dispatcherHandler)
-                        .filters(filters)
-                        .exceptionHandlers(new ResponseStatusExceptionHandler())
-                        .build();
-
-        /*
-         * Initialization HttpServer
-         */
-        final HttpServer server = new ReactorHttpServer();
-        server.setPort(8080);
-        server.setHandler(httpHandler);
-        server.afterPropertiesSet();
-        server.start();
-
-        /*
-         * Server listening wait and shutdown setup
-         */
-        CompletableFuture<Void> stop = new CompletableFuture<>();
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            stop.complete(null);
-        }));
-        synchronized (stop) {
-            stop.wait();
-        }
-
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
-
-
-    private Application() {
-        super();
-    }
-
 
 }
