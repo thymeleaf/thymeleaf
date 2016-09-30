18c64ca9ae0b9d1fc9f7ddc4845a6e4ad961a853
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-10-01 01:43:08 +0200

    Minor comment improvements

--- src/main/java/thymeleafexamples/sayhello/dialect/HelloDialect.java
+++ src/main/java/thymeleafexamples/sayhello/dialect/HelloDialect.java
@@ -28,12 +28,20 @@ import org.thymeleaf.processor.IProcessor;
 public class HelloDialect extends AbstractProcessorDialect {
 
     public HelloDialect() {
-        super("Hello Dialect", "hello", 1000);
+        super(
+                "Hello Dialect",    // Dialect name
+                "hello",            // Dialect prefix (hello:*)
+                1000);              // Dialect precedence
     }
 
     
     /*
-     * The processors.
+     * Initialize the dialect's processors.
+     *
+     * Note the dialect prefix is passed here because, although we set
+     * "hello" to be the dialect's prefix at the constructor, that only
+     * works as a default, and at engine configuration time the user
+     * might have chosen a different prefix to be used.
      */
     public Set<IProcessor> getProcessors(final String dialectPrefix) {
         final Set<IProcessor> processors = new HashSet<IProcessor>();
