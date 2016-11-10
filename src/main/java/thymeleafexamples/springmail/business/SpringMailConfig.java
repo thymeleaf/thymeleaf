68e3b81cde47ffc83af81d904aaa6b857c6f27c1
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-11-11 00:51:39 +0100

    Improved email configuration comments

--- src/main/java/thymeleafexamples/springmail/business/SpringMailConfig.java
+++ src/main/java/thymeleafexamples/springmail/business/SpringMailConfig.java
@@ -108,9 +108,13 @@ public class SpringMailConfig implements ApplicationContextAware, EnvironmentAwa
     @Bean
     public TemplateEngine emailTemplateEngine() {
         final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        // Resolver for TEXT emails
         templateEngine.addTemplateResolver(textTemplateResolver());
+        // Resolver for HTML emails (except the editable one)
         templateEngine.addTemplateResolver(htmlTemplateResolver());
+        // Resolver for HTML editable emails (which will be treated as a String)
         templateEngine.addTemplateResolver(stringTemplateResolver());
+        // Message source, internationalization specific to emails
         templateEngine.setTemplateEngineMessageSource(emailMessageSource());
         return templateEngine;
     }
