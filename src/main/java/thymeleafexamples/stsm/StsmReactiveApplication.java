4e30c0d2788f4a85ba72c14c8538557df7779c36
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-12-09 00:23:50 +0100

    Added reactive-enabled configuration and controller

--- src/main/java/thymeleafexamples/stsm/StsmReactiveApplication.java
+++ src/main/java/thymeleafexamples/stsm/StsmReactiveApplication.java
@@ -1,4 +1,4 @@
-package thymeleafexamples;
+package thymeleafexamples.stsm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
