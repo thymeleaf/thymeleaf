5c8229a9afd6adaf009b2f34cce00a479441cec2
Daniel Fernández <daniel.fernandez@11thlabs.org>
2017-11-05 17:35:44 +0100

    Updated site to 3.0.9 and 2.1.6 (pending announcement links)

--- src/site/scripts/thymeleaf.js
+++ src/site/scripts/thymeleaf.js
@@ -59,7 +59,7 @@
 			'<dependency>',
 			'    <groupId>org.thymeleaf</groupId>',
 			'    <artifactId>thymeleaf</artifactId>',
-			'    <version>3.0.8.RELEASE</version>',
+			'    <version>3.0.9.RELEASE</version>',
 			'</dependency>'
 		].join('\n'),
 
@@ -162,7 +162,7 @@
 			'<dependency>',
 			'  <groupId>org.thymeleaf</groupId>',
 			'  <artifactId>thymeleaf</artifactId>',
-			'  <version>3.0.9-SNAPSHOT</version>',
+			'  <version>3.0.10-SNAPSHOT</version>',
 			'  <scope>compile</scope>',
 			'</dependency>'
 		].join('\n')
