c9b6c05e40c26e2fd1c74139aac73f6237cd547d
Daniel Fernández <daniel.fernandez@11thlabs.org>
2021-12-17 01:30:14 +0100

    Cleaned imports

--- src/main/java/org/thymeleaf/web/servlet/IServletWebApplication.java
+++ src/main/java/org/thymeleaf/web/servlet/IServletWebApplication.java
@@ -27,8 +27,6 @@ import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import org.thymeleaf.util.Validate;
 import org.thymeleaf.web.IWebApplication;
