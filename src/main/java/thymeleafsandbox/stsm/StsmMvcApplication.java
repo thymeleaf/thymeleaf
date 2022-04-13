b13b8200a1944b6a3efbd67feadd7b48b2838e57
Daniel Fernández <daniel.fernandez@11thlabs.org>
2022-04-13 15:29:59 +0200

    Created orphaned branch of thymeleafsandbox-stsm-mvc for spring5

---
+++ src/main/java/thymeleafsandbox/stsm/StsmMvcApplication.java
@@ -0,0 +1,12 @@
+package thymeleafsandbox.stsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsmMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsmMvcApplication.class, args);
+	}
+}
