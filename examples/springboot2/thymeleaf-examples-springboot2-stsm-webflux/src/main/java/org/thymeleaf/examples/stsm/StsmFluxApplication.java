904b2605a54bef2314a2ebced91689c6e481cd7c
Daniel Fernández <daniel.fernandez@11thlabs.org>
2022-06-26 02:27:53 +0200

    Refactored thymeleafsandbox-springboot2 applications to "org.thymeleaf.examples" package

---
+++ examples/springboot2/thymeleaf-examples-springboot2-stsm-webflux/src/main/java/org/thymeleaf/examples/stsm/StsmFluxApplication.java
@@ -0,0 +1,12 @@
+package org.thymeleaf.examples.stsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsmFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsmFluxApplication.class, args);
+	}
+}
