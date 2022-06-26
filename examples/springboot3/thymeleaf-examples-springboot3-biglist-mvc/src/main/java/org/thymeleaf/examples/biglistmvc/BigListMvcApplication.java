735293eebdb4bd6fff2f340a1684f557d8335114
Daniel Fernández <daniel.fernandez@11thlabs.org>
2022-06-26 02:28:29 +0200

    Refactored thymeleafsandbox-springboot3 applications to "org.thymeleaf.examples" package

---
+++ examples/springboot3/thymeleaf-examples-springboot3-biglist-mvc/src/main/java/org/thymeleaf/examples/biglistmvc/BigListMvcApplication.java
@@ -0,0 +1,12 @@
+package org.thymeleaf.examples.biglistmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigListMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigListMvcApplication.class, args);
+	}
+}
