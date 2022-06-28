56c8de896e3a2efca280d07a2f8e277a3e19472d
Daniel Fernández <daniel.fernandez@11thlabs.org>
2022-06-29 00:09:50 +0200

    Moved code in springboot2 example applications to the org.thymeleaf.examples.springboot2 package

---
+++ examples/springboot2/thymeleaf-examples-springboot2-sse-webflux/src/main/java/org/thymeleaf/examples/springboot2/sse/webflux/SSEWebFluxApplication.java
@@ -0,0 +1,12 @@
+package org.thymeleaf.examples.springboot2.sse.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SSEWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SSEWebFluxApplication.class, args);
+	}
+}
---
+++ examples/springboot2/thymeleaf-examples-springboot2-stsm-webflux/src/main/java/org/thymeleaf/examples/springboot2/stsm/webflux/StsmWebFluxApplication.java
@@ -0,0 +1,12 @@
+package org.thymeleaf.examples.springboot2.stsm.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StsmWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsmWebFluxApplication.class, args);
+	}
+}
