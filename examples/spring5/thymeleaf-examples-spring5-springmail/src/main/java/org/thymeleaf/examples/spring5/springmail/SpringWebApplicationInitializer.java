ad413cf81ad5ca0ebca8390561e273ebbb05b277
Daniel Fernández <daniel.fernandez@11thlabs.org>
2022-06-28 20:43:56 +0200

    Moved code in spring5 example applications to the org.thymeleaf.examples.spring5 package

---
+++ examples/spring5/thymeleaf-examples-spring5-springmail/src/main/java/org/thymeleaf/examples/spring5/springmail/SpringWebApplicationInitializer.java
@@ -0,0 +1,45 @@
+package org.thymeleaf.examples.spring5.springmail;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.thymeleaf.examples.spring5.springmail.business.SpringBusinessConfig;
+import org.thymeleaf.examples.spring5.springmail.business.SpringMailConfig;
+import org.thymeleaf.examples.spring5.springmail.web.SpringWebConfig;
+
+public class SpringWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    public static final String CHARACTER_ENCODING = "UTF-8";
+
+
+    public SpringWebApplicationInitializer() {
+        super();
+    }
+
+
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] { SpringWebConfig.class };
+    }
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] { SpringBusinessConfig.class, SpringMailConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        final CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding(CHARACTER_ENCODING);
+        encodingFilter.setForceEncoding(true);
+        return new Filter[] { encodingFilter };
+    }
+
+}
