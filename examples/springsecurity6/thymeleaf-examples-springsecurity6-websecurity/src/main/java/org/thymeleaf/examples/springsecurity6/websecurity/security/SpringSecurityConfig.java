d7245ecad4b46ac584a74d057679b648559a65d4
Daniel Fernández <daniel.fernandez@11thlabs.org>
2022-10-12 02:18:03 +0200

    Added websecurity example application for springsecurity6

---
+++ examples/springsecurity6/thymeleaf-examples-springsecurity6-websecurity/src/main/java/org/thymeleaf/examples/springsecurity6/websecurity/security/SpringSecurityConfig.java
@@ -0,0 +1,69 @@
+/*
+ * =============================================================================
+ *
+ *   Copyright (c) 2011-2016, The THYMELEAF team (http://www.thymeleaf.org)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ * =============================================================================
+ */
+package org.thymeleaf.examples.springsecurity6.websecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+    public SpringSecurityConfig() {
+        super();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+        http
+                .formLogin()
+                .loginPage("/login.html")
+                .failureUrl("/login-error.html")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/index.html")
+                .and()
+                .authorizeHttpRequests()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/user/**").hasRole("USER")
+                .requestMatchers("/shared/**").hasAnyRole("USER","ADMIN")
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/403.html");
+        return http.build();
+    }
+
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("jim").password("{noop}demo").roles("ADMIN").build(),
+                User.withUsername("bob").password("{noop}demo").roles("USER").build(),
+                User.withUsername("ted").password("{noop}demo").roles("USER","ADMIN").build());
+    }
+
+
+}
